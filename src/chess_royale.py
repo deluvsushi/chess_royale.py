1cde0be8f20ec84a6fa196895ba78f6606d265d5
ecclesias <77536370+zeviel@users.noreply.github.com>
2022-11-19 20:08:45 +0800

    Update chess_royale.py

--- src/chess_royale.py
+++ src/chess_royale.py
@@ -4,7 +4,7 @@ from os import urandom
 from hashlib import md5
 
 class ChessRoyale:
-	def __init__(self, locale: str = "en"):
+	def __init__(self, locale: str = "en") -> None:
 		self.first_api = "https://master.chessroyale.app/api/v1"
 		self.second_api = "https://api-v1-master.chessroyale.app"
 		self.news_api = "https://api-news.whitesharx.app/starfall"
@@ -21,7 +21,7 @@ class ChessRoyale:
 		self.idfa = f"{uuid4()}"
 		self.idfv = md5(urandom(15)).hexdigest()
 
-	def login_as_guest(self):
+	def login_as_guest(self) -> dict:
 		data = {
 			"locale": self.locale,
 			"idfa": self.idfa,
@@ -38,7 +38,7 @@ class ChessRoyale:
 			self.headers["authorization"] = self.auth_token
 		return response
 
-	def login_with_auth_token(self, auth_token: str):
+	def login_with_auth_token(self, auth_token: str) -> dict:
 		self.auth_token = auth_token
 		self.headers["authorization"] = f"Bearer {self.auth_token}"
 		response = self.get_current_player()
@@ -47,43 +47,43 @@ class ChessRoyale:
 			self.friend_code = response["player"]["code"]
 		return response
 
-	def check_version(self, version: str):
+	def check_version(self, version: str) -> dict:
 		return requests.get(
 			f"{self.first_api}/util/version/check?version={version}",
 			headers=self.headers).json()
 
-	def get_settings(self):
+	def get_settings(self) -> dict:
 		return requests.get(
 			F"{self.first_api}/settings",
 			headers=self.headers).json()
 
-	def get_current_player(self):
+	def get_current_player(self) -> dict:
 		return requests.get(
 			f"{self.first_api}/players/me",
 			headers=self.headers).json()
 
-	def get_current_time(self):
+	def get_current_time(self) -> dict:
 		return requests.get(
 			f"{self.first_api}/util/time/current",
 			headers=self.headers).json()
 
-	def get_current_olympiads(self):
+	def get_current_olympiads(self) -> dict:
 		return requests.get(
 			f"{self.first_api}/olympiads/current",
 			headers=self.headers).json()
 
 
-	def get_previous_olympiads(self):
+	def get_previous_olympiads(self) -> dict:
 		return requests.get(
 			f"{self.first_api}/olympiads/previous",
 			headers=self.headers).json()
 
-	def get_clubs_list(self):
+	def get_clubs_list(self) -> dict:
 		return requests.get(
 			f"{self.first_api}/clubs",
 			headers=self.headers).json()
 
-	def get_current_daily_mission(self):
+	def get_current_daily_mission(self) -> dict:
 		return requests.get(
 			f"{self.first_api}/daily_missions/current",
 			headers=self.headers).json()
@@ -91,22 +91,22 @@ class ChessRoyale:
 	def get_plays_list(
 			self,
 			is_finished: bool = True,
-			count: int = 10):
+			count: int = 10) -> dict:
 		return requests.get(
 			f"{self.first_api}/plays?isFinished={is_finished}&count={count}",
 			headers=self.headers).json()
 
-	def get_current_rivals(self):
+	def get_current_rivals(self) -> dict:
 		return requests.get(
 			f"{self.first_api}/rivals/current",
 			headers=self.headers).json()
 
-	def get_player_info(self, player_id: str):
+	def get_player_info(self, player_id: str) -> dict:
 		return requests.get(
 			f"{self.first_api}/players/{player_id}",
 			headers=self.headers).json()
 
-	def get_player_achievements(self, player_id: str):
+	def get_player_achievements(self, player_id: str) -> dict:
 		return requests.get(
 			f"{self.first_api}/simple_achievements/{player_id}",
 			headers=self.headers).json()
@@ -114,122 +114,132 @@ class ChessRoyale:
 	def get_leaderboard(
 			self,
 			type: str = "world",
-			path: str = "clubPoints"):
+			path: str = "clubPoints") -> dict:
 		return requests.get(
 			f"{self.first_api}/leaderboard/{type}?path={path}",
 			headers=self.headers).json()
 
-	def get_storm_leaderboard(self):
+	def get_storm_leaderboard(self) -> dict:
 		return requests.get(
 			f"{self.first_api}/storm/leaderboard/count",
 			headers=self.headers).json()
 
-	def get_today_storm_leaderboard(self):
+	def get_today_storm_leaderboard(self) -> dict:
 		return requests.get(
 			f"{self.first_api}/storm/leaderboard/count/today",
 			headers=self.headers).json()
 
-	def get_series_storm_leaderboard(self):
+	def get_series_storm_leaderboard(self) -> dict:
 		return requests.get(
 			f"{self.first_api}/storm/leaderboard/count/series",
 			headers=self.headers).json()
 
-	def get_puzzles_leaderboard(self, type: str = "world"):
+	def get_puzzles_leaderboard(self, type: str = "world") -> dict:
 		return requests.get(
 			f"{self.first_api}/puzzles/leaderboard/{type}",
 			headers=self.headers).json()
 
-	def get_achievements_list(self):
+	def get_achievements_list(self) -> dict:
 		return requests.get(
 			f"{self.second_api}/achievements",
 			headers=self.headers).json()
 
-	def change_flag(self, flag_icon: int):
-		data = {"flagIcon": flag_icon}
+	def change_flag(self, flag_icon: int) -> dict:
+		data = {
+			"flagIcon": flag_icon
+		}
 		return requests.put(
 			f"{self.first_api}/players/me/flag",
 			json=data,
 			headers=self.headers).json()
 
-	def change_nickname(self, nickname: str):
-		data = {"nickname": nickname}
+	def change_nickname(self, nickname: str) -> dict:
+		data = {
+			"nickname": nickname
+		}
 		return requests.put(
 			f"{self.first_api}/players/me/nickname",
 			json=data,
 			headers=self.headers).json()
 
-	def get_news_list(self):
+	def get_news_list(self) -> dict:
 		return requests.get(
 			f"{self.news_api}/{self.locale}/articles",
 			headers=self.headers).json()
 
-	def get_coin_reward(self, reward_number: int):
+	def get_coin_reward(self, reward_number: int) -> dict:
 		return requests.patch(
 			f"{self.second_api}/rewarded/watch/{reward_number}",
 			headers=self.headers).json()
 
-	def spin_wheel(self):
+	def spin_wheel(self) -> dict:
 		return requests.get(
 			f"{self.first_api}/wheel/ad/twist",
 			headers=self.headers).json()
 
-	def get_shop(self):
+	def get_shop(self) -> dict:
 		return requests.get(
 			f"{self.first_api}/shop",
 			headers=self.headers).json()
 			
-	def get_shop_coins(self):
+	def get_shop_coins(self) -> dict:
 		return requests.get(
 			f"{self.first_api}/shop/coins",
 			headers=self.headers).json()
 			
-	def get_shop_hints(self):
+	def get_shop_hints(self) -> dict:
 		return requests.get(
 			f"{self.first_api}/shop/hints",
 			headers=self.headers).json()
 
-	def get_shop_avatars(self):
+	def get_shop_avatars(self) -> dict:
 		return requests.get(
 			f"{self.first_api}/shop/avatars",
 			headers=self.headers).json()
 	
-	def get_shop_phrases(self):
+	def get_shop_phrases(self) -> dict:
 		return requests.get(
 			f"{self.first_api}/shop/hints/phrases",
 			headers=self.headers).json()
 	
-	def get_shop_emoticons(self):
+	def get_shop_emoticons(self) -> dict:
 		return requests.get(
 			f"{self.first_api}/shop/emoticons",
 			headers=self.headers).json()
 	
-	def get_shop_boosters(self):
+	def get_shop_boosters(self) -> dict:
 		return requests.get(
 			f"{self.first_api}/shop/boosters",
 			headers=self.headers).json()
 	
-	def get_shop_boards(self):
+	def get_shop_boards(self) -> dict:
 		return requests.get(
 			f"{self.first_api}/shop/boards",
 			headers=self.headers).json()
 	
-	def get_shop_safes(self):
+	def get_shop_safes(self) -> dict:
 		return requests.get(
 			f"{self.first_api}/shop/safes",
 			headers=self.headers).json()
 	
-	def get_shop_passes(self):
+	def get_shop_passes(self) -> dict:
 		return requests.get(
 			f"{self.first_api}/shop/passes",
 			headers=self.headers).json()
 	
 
-	def buy_item(self, category: str, item_id: str):
+	def buy_item(
+			self,
+			category: str,
+			item_id: str) -> dict:
 		return requests.post(
 			f"{self.first_api}/shop/{category}/{item_id}",
 			headers=self.headers).json()
 
-	def claim_achievement(self, type: str, degree: int):
+	def claim_achievement(
+			self,
+			type: str,
+			degree: int) -> dict:
 		data = {
 			"type": type,
 			"degree": degree
@@ -239,7 +249,7 @@ class ChessRoyale:
 			json=data,
 			headers=self.headers).json()
 
-	def change_image_url(self, image_url: str):
+	def change_image_url(self, image_url: str) -> dict:
 		data = {
 			"imageUrl": image_url
 		}
